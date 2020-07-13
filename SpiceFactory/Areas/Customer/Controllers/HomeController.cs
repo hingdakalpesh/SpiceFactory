6fc4a312db0e05167b60b27531f8392052294a68
Kalpesh Hingda <hingdakalpesh@hotmail.com>
2020-07-13 11:14:51 +0100

    Indentity Scaffolding

--- SpiceFactory/Areas/Customer/Controllers/HomeController.cs
+++ SpiceFactory/Areas/Customer/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using SpiceFactory.Models;
 
 namespace SpiceFactory.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
---
+++ SpiceFactory/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SpiceFactory.Data;
+
+[assembly: HostingStartup(typeof(SpiceFactory.Areas.Identity.IdentityHostingStartup))]
+namespace SpiceFactory.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
